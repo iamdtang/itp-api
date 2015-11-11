300b21d24e1603bef01a5cdb5c5db11f4e5677fd
dtang <dtang@edgecast.com>
2015-11-10 21:04:04 -0800

    refactor to use ES6

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
@@ -13,25 +15,11 @@ app.use(function(req, res, next) {
 });
 
 app.get('/songs', function(req, res) {
-  var songs = Song.findAll();
-  var artists;
-
-  if (req.query.withArtists) {
-    artists = Artist.findAll();
-
-    Q.all([songs, artists]).then(function(data) {
-      res.json({
-        songs: data[0],
-        artists: data[1]
-      });
+  Song.findAll().then((results) => {
+    res.json({
+      songs: results
     });
-  } else {
-    songs.then(function(results) {
-      res.json({
-        songs: results
-      });
-    });
-  }
+  });
 });
 
 app.get('/songs/:id', function(req, res) {
@@ -51,52 +39,47 @@ app.get('/songs/:id', function(req, res) {
 });
 
 app.get('/artists', function(req, res) {
-  Artist.findAll().then(function(results) {
-    res.json({
-      artists: results
+  Artist.findAll().then((artists) => {
+    return res.json({
+      artists: artists
     });
   });
 });
 
 app.get('/artists/:id', function(req, res) {
-  'use strict';
-
-  console.log(req.params.id);
-  var songQuery = Song.findAll({
+  let songQuery = Song.findAll({
     where: {
       artistId: req.params.id
     }
   });
 
-  var artistQuery = Artist.findById(req.params.id);
+  let artistQuery = Artist.findById(req.params.id);
 
-  Q.all([ songQuery, artistQuery ]).then(function(data) {
-    var artist = data[1];
+  Q.all([ songQuery, artistQuery ]).then((data) => {
+    let artist = data[1];
 
     if (!artist) {
-      res.status(404).json({
+      return res.status(404).json({
         errors: [
           { status: "404", id: "ARTIST_NOT_FOUND", detail: "Artist not found" }
         ]
       });
-    } else {
-      let response = {
-        artist: {
-          id: artist.id,
-          artistName: artist.artistName,
-          songs: data[0].map((song) => {
-            return song.id;
-          })
-        }
-      }
-
-      res.json(response);
     }
+
+    return res.json({
+      artist: {
+        id: artist.id,
+        artistName: artist.artistName,
+        songs: data[0].map((song) => {
+          return song.id;
+        })
+      }
+    });
   });
 });
 
 app.get('/genres', function(req, res) {
-  Genre.findAll().then(function(results) {
+  Genre.findAll().then((results) => {
     res.json({
       genres: results
     });
