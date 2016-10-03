4078d58c70feb6b8125c68bf1a483ddeb98b1d9c
David Tang <dtang859@gmail.com>
2016-10-03 00:10:44 -0700

    update

--- app2.js
+++ app2.js
@@ -1,6 +1,8 @@
 const app = require('koa')();
 const router = require('koa-router')();
 const Genre = require('./models/Genre');
+const Artist = require('./models/Artist');
+const Song = require('./models/Song');
 
 router.get('/api/genres', function *(next) {
   this.body = yield Genre.findAll();
